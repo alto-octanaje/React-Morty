4c61f0c80a560b882f35fbca97384b9ed5698e65
jeisson-olwen <jeissonolwen@gmail.com>
2023-06-16 17:20:08 -0500

    confi-Backend-findID_DB||api

--- server/src/Controllers/ControllerUser/ControllerUser.js
+++ server/src/Controllers/ControllerUser/ControllerUser.js
@@ -1,7 +1,16 @@
-const {user} =require('../../db')
+const { user } = require("../../db");
+const axios = require("axios");
 
-const CreateUser= async(name)=>{
-    return  await user.create({name})
+const CreateUser = async (name) => {
+  return await user.create({ name });
+};
+const idUser = async (id, source ) => {
+    console.log(id,"",source);
+    const miUser=
+  source === "api"
+    ?  (await axios.get(`https://rickandmortyapi.com/api/character/${id}`)).data
+    : await user.findByPk(id);
 
-}
-module.exports={CreateUser}
+return miUser
+};
+module.exports = { CreateUser, idUser };
--- server/src/handler/users/HandlerUsers.js
+++ server/src/handler/users/HandlerUsers.js
@@ -1,5 +1,6 @@
 const {
   CreateUser,
+  idUser,
 } = require("../../Controllers/ControllerUser/ControllerUser");
 
 const getApiUsers = (req, res) => {
@@ -7,17 +8,25 @@ const getApiUsers = (req, res) => {
   if (name) res.send(`esta ruta trae todos los datos que tengan ${name} `);
   else res.send("esta ruta trae todos los datos de la api si no hay quere");
 };
-const getUsersId = (req, res) => {
-  const { id } = req.params;
-  res.send(`esta ruta trae todos los datos del user ${id} `);
+const getUsersId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const source = isNaN(id) ? "DB" : "api";
+    
+    const findID = await idUser(id, source);
+    console.log(findID);
+    res.status(200).json(findID);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 const postUser = async (req, res) => {
   try {
     const { name } = req.body;
-    const newUser = await CreateUser( name );
-    res.status(201).json(newUser)
+    const newUser = await CreateUser(name);
+    res.status(201).json(newUser);
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
 };
 
