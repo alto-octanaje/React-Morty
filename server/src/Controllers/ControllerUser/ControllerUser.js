8e6e9730e08a25f7f96d04d9d4650f90c0d37ba3
jeisson-olwen <jeissonolwen@gmail.com>
2023-06-16 18:30:44 -0500

    confi-Backend-Get-All-wihtMyprops

--- server/src/Controllers/ControllerUser/ControllerUser.js
+++ server/src/Controllers/ControllerUser/ControllerUser.js
@@ -1,22 +1,27 @@
 const { user } = require("../../db");
 const axios = require("axios");
 
+const cleanArray = (array) =>
+  array.map((e) => ({
+    id: e.id,
+    name: e.name,
+  }));
+
 const getAllUsers = async () => {
-  const userApi= (await axios.get("https://rickandmortyapi.com/api/character/")).data.results
-  const userDB = await user.findAll()
-
-  return [...userApi,...userDB];
+  const userApiRaw = (
+    await axios.get("https://rickandmortyapi.com/api/character/")
+  ).data.results;
+  const userDB = await user.findAll();
+  const userApi = cleanArray(userApiRaw);
+  return [...userApi, ...userDB];
 };
-const searchUserName = (name) => {
-
-};
+const searchUserName = (name) => {};
 
 const CreateUser = async (name) => {
   return await user.create({ name });
 };
 
 const idUser = async (id, source) => {
-
   const miUser =
     source === "api"
       ? (await axios.get(`https://rickandmortyapi.com/api/character/${id}`))
@@ -25,4 +30,4 @@ const idUser = async (id, source) => {
 
   return miUser;
 };
-module.exports = { CreateUser, idUser,getAllUsers,searchUserName };
+module.exports = { CreateUser, idUser, getAllUsers, searchUserName };
