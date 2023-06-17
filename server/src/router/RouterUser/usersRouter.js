443d8fdd2809eedf641cffc9d1e62205f545dfe2
jeisson-olwen <jeissonolwen@gmail.com>
2023-06-17 11:12:58 -0500

    confi-Backend-validateUser-post

--- server/src/router/RouterUser/usersRouter.js
+++ server/src/router/RouterUser/usersRouter.js
@@ -1,18 +1,14 @@
 const {Router} =require("express")
-const { getApiUsers, getUsersId,postUser }= require("../../handler/users/HandlerUsers")
+const { getApiUsers, getUsersId,postUser }= require("../../handler/users/HandlerUsers");
+const { crateUserV } = require("../../validate/userValidate/userValidate");
 
 const usersRouter= Router();
 
-const validate=(req,res,next)=>{
-    const {name}= req.body;
-    if(!name) return res.status(400).json({error:"Missing Name" })
-    next();
-}
 
 
 usersRouter.get("/", getApiUsers)
 
 usersRouter.get("/:id",getUsersId)
-usersRouter.post("/",validate, postUser)
+usersRouter.post("/",crateUserV, postUser)
 
 module.exports= usersRouter;
